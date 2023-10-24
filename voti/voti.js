e68898c78705aab405d4d38dc7ba8e0cdc6c36cd
Nadina <sheriefnadina@gmail.com>
2023-10-24 09:45:04 +0200

    feat/voti

---
+++ voti/voti.js
@@ -0,0 +1,5 @@
+let voto1 = prompt('Che voto hai preso?')
+let voto2 = prompt('Che voto hai preso?')
+let voto3 = prompt('Che voto hai preso?')
+let result = (voto1 + voto2 + voto3) / 3
+alert('La vostra media è ' + result)
