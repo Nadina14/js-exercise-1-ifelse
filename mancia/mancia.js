76d504feb78b417827d94b2f95f309ea504eaa72
Nadina <sheriefnadina@gmail.com>
2023-10-24 10:03:33 +0200

    feat/mancia

---
+++ mancia/mancia.js
@@ -0,0 +1,4 @@
+let conto = prompt('Qual è il totale del conto?')
+let mancia = prompt('Che percentale di mancia hai selezionato?')
+let result = (conto * mancia) / 100
+alert('Hai dato ' + result + '€ di mancia')
--- voti/voti.js
+++ voti/voti.js
@@ -1,5 +1,8 @@
 let voto1 = prompt('Che voto hai preso?')
+let voto11 = Number(voto1)
 let voto2 = prompt('Che voto hai preso?')
+let voto12 = Number(voto2)
 let voto3 = prompt('Che voto hai preso?')
-let result = (voto1 + voto2 + voto3) / 3
+let voto13 = Number(voto3)
+let result = (voto11 + voto12 + voto13) / 3
 alert('La vostra media è ' + result)
