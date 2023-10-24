ba8e1e87275fda70b1c219ea101fd37591a8663d
Nadina <sheriefnadina@gmail.com>
2023-10-24 09:27:23 +0200

    feat/ creazione primi due es

---
+++ caffè/main.js
@@ -0,0 +1,3 @@
+let caffè = prompt('Quante tazze di caffè hai venduto oggi?')
+let result = 2 * caffè + '€'
+alert('Hai guadagnato ' + result)
---
+++ età/età.js
@@ -0,0 +1,3 @@
+let età = prompt('Qual è il tuo anno di nascita?')
+let result = 2023 - età
+alert('Hai ' + result + ' anni')
